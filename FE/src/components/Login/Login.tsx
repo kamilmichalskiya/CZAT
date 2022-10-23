be490a637adaf533525de27969c44e667ec1f71a
Oxozon <oxozon.o4@gmail.com>
2022-10-23 19:20:43 +0200

    add loader component

--- FE/src/components/Login/Login.tsx
+++ FE/src/components/Login/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 import { useActions } from '../../hooks/useActions';
 import {
@@ -15,13 +15,28 @@ import {
 import { PersonFill } from '@styled-icons/bootstrap/PersonFill';
 import { Lock } from '@styled-icons/fa-solid/Lock';
 import { EyeOutline } from '@styled-icons/evaicons-outline/EyeOutline';
+import { toast } from 'react-toastify';
+import Loader from '../Loader/Loader';
 // import '@fontsource/montserrat';
 
-const Login = () => {
+const Login: React.FC = () => {
   const [userLogin, setUserLogin] = useState('');
   const [userPassword, setUserPassword] = useState('');
   const [passwordShown, setPasswordShown] = useState(false);
   const { data, error, loading } = useTypedSelector((state) => state.login);
+
+  useEffect(() => {
+    const { search: queryParams } = window.location;
+    if (queryParams === '?error=true') {
+      toast.error('Podane dane logowania są nieprawidłowe.', { toastId: 'toast-id-error' });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof error === 'string') {
+      toast.error(error, { toastId: 'toast-id-login-error' });
+    }
+  }, [error]);
 
   const togglePassword = () => {
     setPasswordShown(!passwordShown);
@@ -42,6 +57,7 @@ const Login = () => {
 
   return (
     <LoginWrapper>
+      <Loader isLoading={loading} />
       <LoginTitle>
         Witaj w <GreenTextWrapper>CZAT</GreenTextWrapper>!
       </LoginTitle>
--- FE/src/templates/Root.tsx
+++ FE/src/templates/Root.tsx
@@ -4,12 +4,25 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../assets/styles/GlobalStyle';
 import { theme } from '../assets/styles/theme';
 import Login from '../components/Login/Login';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Root = () => {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
         <Login />
       </ThemeProvider>
     </Provider>
