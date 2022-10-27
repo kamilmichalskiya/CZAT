1d27e660198417e38f65b9ddecc43ed10f6afaa4
p-olszewski <przemo1297@gmail.com>
2022-10-27 21:28:18 +0200

    Create layout grid

--- FE/src/components/Chat/Chat-styled.ts
+++ FE/src/components/Chat/Chat-styled.ts
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
+import { StyledIconBase } from '@styled-icons/styled-icon';
 
 export const ChatWrapper = styled.div`
-  min-height: 100vh;
+  /* min-height: 100vh; */
+  height: 900px;
   max-width: ${({ theme }) => theme.size.xl};
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 0.35fr 0.65fr;
+  grid-template-rows: 0.15fr 0.85fr;
+  grid-column-gap: 10px;
+  grid-row-gap: 10px;
   padding: 35px 10px 25px;
   background-color: ${({ theme }) => theme.colors.secondary};
   color: ${({ theme }) => theme.colors.white};
@@ -26,10 +31,110 @@ export const ChatWrapper = styled.div`
   }
 `;
 
+export const IconStyleWrapper = styled.div`
+  ${StyledIconBase} {
+    color: ${({ theme }) => theme.colors.white};
+  }
+  &:hover {
+    opacity: 0.9;
+    cursor: pointer;
+  }
+`;
+
+export const InactiveIconStyleWrapper = styled(IconStyleWrapper)`
+  ${StyledIconBase} {
+    color: ${({ theme }) => theme.colors.placeholder};
+  }
+  &:hover {
+    cursor: default;
+  }
+`;
+
 export const ChatHeader = styled.div`
-  width: 100%;
-  margin: 10px 0 10px 5px;
+  /* width: 100%; */
+  /* margin: 10px 0 10px 5px; */
+  grid-area: 1 / 1 / 2 / 3;
   font-size: ${({ theme }) => theme.fontSize.xxl};
   color: ${({ theme }) => theme.colors.primary};
   text-align: left;
+  font-weight: 600;
+`;
+
+export const LeftPanel = styled.div`
+  grid-area: 2 / 1 / 3 / 2;
+  /* background-color: ${({ theme }) => theme.colors.grey}; */
+  border-radius: 10px;
+  border: 2px solid ${({ theme }) => theme.colors.grey};
+  padding: 10px;
+`;
+
+export const SearchBarWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 10px;
+  /* ::placeholder {
+    color: ${({ theme }) => theme.colors.white};
+    font-size: ${({ theme }) => theme.fontSize.xl};
+    letter-spacing: 0.08em;
+  } */
+`;
+
+export const ListElementWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+  margin-bottom: 10px;
+  background-color: ${({ theme }) => theme.colors.grey};
+  width: 100%;
+  height: 4em;
+  border-radius: 10px;
+`;
+
+export const ListPhotoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  font-size: 2em;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const ListContainerWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  margin-left: 15px;
+  flex-grow: 1;
+  height: 100%;
+  font-size: ${({ theme }) => theme.fontSize.m};
+  color: ${({ theme }) => theme.colors.lightGrey};
+`;
+
+export const ListContainerUpperRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ListContainerUsername = styled.div`
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const ListContainerLastMsgTime = styled.div`
+  font-weight: initial;
+  font-size: ${({ theme }) => theme.fontSize.s};
+`;
+
+export const ListContainerMessage = styled.div`
+  font-weight: initial;
+`;
+
+export const RightPanel = styled.div`
+  grid-area: 2 / 2 / 3 / 3;
+  background-color: ${({ theme }) => theme.colors.grey};
+  border-radius: 10px;
 `;
--- FE/src/components/Chat/Chat.tsx
+++ FE/src/components/Chat/Chat.tsx
@@ -1,6 +1,21 @@
 import { useEffect } from 'react';
-import { ChatWrapper, ChatHeader } from './Chat-styled';
+import {
+  ChatWrapper,
+  ChatHeader,
+  LeftPanel,
+  SearchBarWrapper,
+  InactiveIconStyleWrapper,
+  ListElementWrapper,
+  ListPhotoWrapper,
+  ListContainerWrapper,
+  ListContainerUpperRow,
+  ListContainerUsername,
+  ListContainerLastMsgTime,
+  ListContainerMessage,
+  RightPanel,
+} from './Chat-styled';
 import { toast } from 'react-toastify';
+import { MagnifyingGlass } from '@styled-icons/entypo/MagnifyingGlass';
 
 const Chat: React.FC = () => {
   useEffect(() => {
@@ -9,7 +24,71 @@ const Chat: React.FC = () => {
 
   return (
     <ChatWrapper>
-      <ChatHeader>Czat</ChatHeader>
+      <ChatHeader>CZAT - Co Za Ambitny Temat</ChatHeader>
+      <LeftPanel>
+        <SearchBarWrapper>
+          <InactiveIconStyleWrapper>
+            <MagnifyingGlass size="24" />
+          </InactiveIconStyleWrapper>
+          <span>Szukaj</span>
+        </SearchBarWrapper>
+        <ListElementWrapper>
+          <ListPhotoWrapper>N</ListPhotoWrapper>
+          <ListContainerUpperRow></ListContainerUpperRow>
+          <ListContainerWrapper>
+            <ListContainerUpperRow>
+              <ListContainerUsername>Nickname123</ListContainerUsername>
+              <ListContainerLastMsgTime>1h ago</ListContainerLastMsgTime>
+            </ListContainerUpperRow>
+            <ListContainerMessage>Lorem ipsum dolor sit amet</ListContainerMessage>
+          </ListContainerWrapper>
+        </ListElementWrapper>
+        <ListElementWrapper>
+          <ListPhotoWrapper>N</ListPhotoWrapper>
+          <ListContainerUpperRow></ListContainerUpperRow>
+          <ListContainerWrapper>
+            <ListContainerUpperRow>
+              <ListContainerUsername>Nickname123</ListContainerUsername>
+              <ListContainerLastMsgTime>1h ago</ListContainerLastMsgTime>
+            </ListContainerUpperRow>
+            <ListContainerMessage>Lorem ipsum dolor sit amet</ListContainerMessage>
+          </ListContainerWrapper>
+        </ListElementWrapper>
+        <ListElementWrapper>
+          <ListPhotoWrapper>N</ListPhotoWrapper>
+          <ListContainerUpperRow></ListContainerUpperRow>
+          <ListContainerWrapper>
+            <ListContainerUpperRow>
+              <ListContainerUsername>Nickname123</ListContainerUsername>
+              <ListContainerLastMsgTime>1h ago</ListContainerLastMsgTime>
+            </ListContainerUpperRow>
+            <ListContainerMessage>Lorem ipsum dolor sit amet</ListContainerMessage>
+          </ListContainerWrapper>
+        </ListElementWrapper>
+        <ListElementWrapper>
+          <ListPhotoWrapper>N</ListPhotoWrapper>
+          <ListContainerUpperRow></ListContainerUpperRow>
+          <ListContainerWrapper>
+            <ListContainerUpperRow>
+              <ListContainerUsername>Nickname123</ListContainerUsername>
+              <ListContainerLastMsgTime>1h ago</ListContainerLastMsgTime>
+            </ListContainerUpperRow>
+            <ListContainerMessage>Lorem ipsum dolor sit amet</ListContainerMessage>
+          </ListContainerWrapper>
+        </ListElementWrapper>
+        <ListElementWrapper>
+          <ListPhotoWrapper>N</ListPhotoWrapper>
+          <ListContainerUpperRow></ListContainerUpperRow>
+          <ListContainerWrapper>
+            <ListContainerUpperRow>
+              <ListContainerUsername>Nickname123</ListContainerUsername>
+              <ListContainerLastMsgTime>1h ago</ListContainerLastMsgTime>
+            </ListContainerUpperRow>
+            <ListContainerMessage>Lorem ipsum dolor sit amet</ListContainerMessage>
+          </ListContainerWrapper>
+        </ListElementWrapper>
+      </LeftPanel>
+      <RightPanel>Right</RightPanel>
     </ChatWrapper>
   );
 };
