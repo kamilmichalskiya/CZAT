f717f4b4e4bd232729d902320ca2e51120eacc7f
kamilmichalskiya <kamilmichalskiya@users.noreply.github.com>
2022-10-29 14:36:12 +0200

    CZAT change config and test

--- be/src/main/java/pl/kmp/be/bm/messages/boundary/MessagesBF.java
+++ be/src/main/java/pl/kmp/be/bm/messages/boundary/MessagesBF.java
@@ -1,6 +1,7 @@
 package pl.kmp.be.bm.messages.boundary;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import pl.kmp.be.api.messages.entity.UiMessage;
 import pl.kmp.be.bm.messages.control.MessagesRepository;
@@ -10,11 +11,13 @@ import pl.kmp.be.bm.messages.entity.Message;
 @Service
 public class MessagesBF {
     private final MessagesRepository repository;
+    private final SimpMessagingTemplate messagingTemplate;
 
     public UiMessage sendMessage(Long chatId, UiMessage uiMessage) {
         final Message message = new Message(uiMessage);
         message.setChatId(chatId);
-        final Message save = repository.save(message);
-        return new UiMessage(save);
+        final UiMessage save = new UiMessage(repository.save(message));
+        messagingTemplate.convertAndSend("/chat/" + chatId, save);
+        return save;
     }
 }
--- be/src/main/java/pl/kmp/be/config/websocket/WebSocketConfig.java
+++ be/src/main/java/pl/kmp/be/config/websocket/WebSocketConfig.java
@@ -3,13 +3,19 @@ package pl.kmp.be.config.websocket;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/queue");
+    }
+
+    @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/queue/");
+        config.enableSimpleBroker("/chat/", "/notification/");
     }
 }
