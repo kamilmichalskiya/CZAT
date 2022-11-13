879e69482765a6adca7cc72ebb3f75c6b39389d4
Oxozon <oxozon.o4@gmail.com>
2022-11-13 11:38:03 +0100

    change login status logic, add mocking comment

--- FE/src/index.tsx
+++ FE/src/index.tsx
@@ -5,6 +5,7 @@ import Root from './templates/Root';
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el!);
 
+// uncoment this code to enable mocking on localhost:3000
 // if (process.env.NODE_ENV === 'development' && window.location.host === 'localhost:3000') {
 //   const { worker } = require('./mocks/browser');
 //   worker.start();
--- FE/src/state/action-creators/loginUser/loginUser.ts
+++ FE/src/state/action-creators/loginUser/loginUser.ts
@@ -22,7 +22,7 @@ export const loginUser = (credentials: any, url: string | undefined) => {
       headers: new Headers({ 'content-type': 'application/json' }),
     };
     const response = await fetch(url, requestOptions);
-    if (response.status === 403 || response.status !== 301) {
+    if (response.status === 403 || (response.status !== 200 && response.status !== 301)) {
       dispatch({
         type: ActionType.LOGIN_USER_ERROR,
         payload: 'Wystąpił problem z żądaniem! Spróbuj ponownie. (CODE: LOGIN_STATUS)',
